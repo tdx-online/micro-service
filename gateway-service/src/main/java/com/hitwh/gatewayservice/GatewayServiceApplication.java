7f82ff8d3c34857773f8c2142a5af18d89008346
Glacier elk <2191710503@qq.com>
2024-06-21 06:45:55 +0800

    init

--- gateway-service/src/main/java/com/hitwh/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/hitwh/gatewayservice/GatewayServiceApplication.java
@@ -1,14 +1,13 @@
-package com.hitwh.productservice;
-
+package com.hitwh.gatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductServiceApplication {
+public class GatewayServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
+        SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
 }
--- product-service/src/main/java/com/hitwh/productservice/controller/CommentController.java
+++ product-service/src/main/java/com/hitwh/productservice/controller/CommentController.java
@@ -1,4 +1,4 @@
-package com.hitwh.productservice.controller;
+package controller;
 
 
 
