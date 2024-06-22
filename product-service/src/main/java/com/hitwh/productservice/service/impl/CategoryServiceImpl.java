bd60021dbcafbec943262ac2e8b7a74ac8249939
Glacier elk <2191710503@qq.com>
2024-06-22 22:50:43 +0800

    k8s yaml

--- product-service/src/main/java/com/hitwh/productservice/service/impl/CategoryServiceImpl.java
+++ product-service/src/main/java/com/hitwh/productservice/service/impl/CategoryServiceImpl.java
@@ -30,7 +30,6 @@ public class CategoryServiceImpl implements CategoryService {
     public Object categoryAndProduct() {
         List<Category> categories = categoryMapper.listAllCategories();
         List<Object> categoryAndProduct = new ArrayList<>();
-//        categories.forEach(System.out::println);
         for (Category category : categories) {
             Map<String, Object> categoryMap = new HashMap<>();
             categoryMap.put("cid", category.getId());
@@ -38,7 +37,6 @@ public class CategoryServiceImpl implements CategoryService {
             categoryMap.put("category_image", category.getUrlPath());
 
             List<ProductDetails> products = productMapper.homePageGetProductsByCategoryId(category.getId());
-//            products.forEach(System.out::println);
             products.forEach(product -> product.setImages(productMapper.getImagesByProductId(product.getId())));
             categoryMap.put("products", products);
 
